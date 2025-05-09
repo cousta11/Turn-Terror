8f137da6f93cefc02217b718c7c296155b08bdce
cousta11 <kostaragulin3626@gmail.com>
2025-05-09 21:22:58 +0300

    fight continion

--- src/fight.c
+++ src/fight.c
@@ -20,7 +20,7 @@ int start_fight(const int y, const int x, int *enemy_y, int *enemy_x,
 }
 int battle_line(int y, int x, int lenght, int hit_zone)
 {
-	int i, hx = rand_to(x + 1, x + lenght - hit_zone);
+	int i, hx = rand_to(x + 1, x + lenght - hit_zone - 1);
 	attrset(COLOR_PAIR(3));
 	mvaddch(y, x, '[');
 	for(i = lenght - 2; i > 0; i--) {
@@ -34,14 +34,22 @@ int battle_line(int y, int x, int lenght, int hit_zone)
 	}
 	return 1;
 }
-void hp_display(int max_y, int max_x, gamer player, int enemy_hp)
+void hp_display(int max_y, int max_x, int player_hp, int enemy_hp)
 {
 	attrset(COLOR_PAIR(4));
-	mvprintw(max_y/2 + 4/2 + 1, max_x/2 - max_x/4, "HP: %d", player.hp);
+	mvprintw(max_y/2 + 4/2 + 1, max_x/2 - max_x/4, "HP: %d", player_hp);
 	addstr("   ");
 	attrset(COLOR_PAIR(2));
 	mvprintw(max_y/2 + 4/2 - 1, max_x/2 - max_x/4, "HP: %d", enemy_hp);
 	addstr("   ");
+}
+void hit(int max_y, int max_x, int player_hp, int enemy_hp, int *mob1, int *mob2)
+{
+	hp_display(max_y, max_x, player_hp, enemy_hp);
+	if(getch() == ' ')
+		*mob1 -= 1;
+	else
+		*mob2 -= 1;
 }
 int fight(int max_y, int max_x, int enemy_y, int enemy_x,
 		gamer *player, int game_place[SIZE][SIZE])
@@ -54,14 +62,14 @@ int fight(int max_y, int max_x, int enemy_y, int enemy_x,
 	battle_line(max_y/2 + 4/2, max_x/2 - max_x/4, max_x/2, hit_zone);
 	timeout(1000);
 	while(enemy_hp > 0 && player->hp > 0) {
-		hp_display(max_y, max_x, *player, enemy_hp);
-		if(getch() == ' ') {
-			enemy_hp -= 1;
-			player->hp -= 1;
-		}
+		hit(max_y, max_x, player->hp, enemy_hp, &enemy_hp, &player->hp);
+		hit(max_y, max_x, player->hp, enemy_hp, &player->hp, &enemy_hp);
+		refresh();
 	}
-	if(player->hp < 1)
+	if(player->hp < 1) {
+		timeout(-1);
 		return 0;
+	}
 	game_place[enemy_y][enemy_x] = SPACE;
 	timeout(-1);
 	return 1;

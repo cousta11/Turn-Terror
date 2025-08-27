6cad00000d65985725d2496f5433a1dfee4e89a2
cousta11 <kostaragulin3626@gmail.com>
2025-08-27 22:36:44 +0300

    menu api

---
+++ src/screen/menu_api.c
@@ -0,0 +1,89 @@
+#include <menu.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include <menu_api.h>
+
+typedef struct choices_t {
+	char *name;
+	struct choices_t *next;
+} choices_t;
+typedef struct menu_t {
+	ITEM **item;
+	WINDOW *w;
+	MENU *menu;
+	choices_t *choice;
+	int n_choices;
+} menu_t;
+
+static void add_choice(choices_t **choice, char *str)
+{
+	choices_t *tmp = malloc(sizeof(choices_t));
+	tmp->name = str;
+	tmp->next = *choice;
+	*choice = tmp;
+}
+/* end arg NULL */
+static void create_choice(menu_t *menu, va_list args)
+{
+	char *str;
+	menu->n_choices = 0;
+	menu->choice = NULL;
+	for(;;) {
+		str = va_arg(args, char *);
+		if(str == NULL)
+			break;
+		menu->n_choices++;
+		add_choice(&(menu->choice), str);
+	}
+}
+static void del_choices(choices_t *choice)
+{
+	if(choice->next != NULL)
+		del_choices((choice)->next);
+	free(choice);
+}
+static choices_t *which_choice(choices_t *choice, int which)
+{
+	int i;
+	for(i = 0; i < which; i++)
+		choice = choice->next;
+	return choice;
+}
+static void create_item(menu_t *menu)
+{
+	int i;
+	menu->item = (ITEM **)calloc(menu->n_choices + 1, sizeof(ITEM *));
+	for(i = 0; i < menu->n_choices; i++)
+		menu->item[i] = new_item(which_choice(menu->choice, i)->name , "");
+}
+menu_t *create_menu(int y, int x, int len_y, int len_x, ...)
+{
+	menu_t *menu = malloc(sizeof(menu_t));
+	va_list args;
+	va_start(args, len_x);
+	create_choice(menu, args);
+	va_end(args);
+	create_item(menu);
+	menu->menu = new_menu(menu->item);
+	menu->w = newwin(len_y, len_x, y, x);
+	set_menu_win(menu->menu, menu->w);
+	set_menu_sub(menu->menu, derwin(menu->w, len_y, len_x, 0, 0));
+	set_menu_mark(menu->menu, "*");
+	post_menu(menu->menu);
+	wrefresh(menu->w);
+	return menu;
+}
+void del_menu(menu_t **menu)
+{
+	int i;
+	unpost_menu((*menu)->menu);
+	free_menu((*menu)->menu);
+	del_choices((*menu)->choice);
+	for(i = 0; i < (*menu)->n_choices; i++)
+		free_item((*menu)->item[i]);
+	free((*menu)->item);
+	delwin((*menu)->w);
+	free(*menu);
+	*menu = NULL;
+}
